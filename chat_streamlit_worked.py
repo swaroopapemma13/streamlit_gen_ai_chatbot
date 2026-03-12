38c5485bd0ae3c192b8ed0b2ded608c0b7867cec
swaroopapemma13 <swaru13@gmail.com>
2026-03-12 08:15:55 +0530

    Create Streamlit chatbot application with ChatGroq
    Implemented a Streamlit application for a generative AI chatbot using ChatGroq. The app includes chat history management and user input handling.

---
+++ chat_streamlit_worked.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv
+import streamlit as st
+from langchain_groq import ChatGroq
+
+
+
+load_dotenv(".env") #load env variables
+
+#streamlit page content
+
+st.set_page_config(
+    page_title="💬 Generative AI Chatbot",
+    page_icon="🤖",
+    layout="centered",
+)
+
+st.title("💬 Generative AI Chatbot")
+#initiate chat history
+
+if "chat_history" not in st.session_state: #initiating chat history
+    st.session_state.chat_history=[]
+
+# show chat history
+for message in st.session_state.chat_history:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+# llm initiate
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    temperature=0.0,
+)
+# input box
+user_prompt = st.chat_input("Ask Chatbot...")
+if user_prompt:
+    st.chat_message("user").markdown(user_prompt)
+    st.session_state.chat_history.append({"role": "user", "content": user_prompt})
+    response = llm.invoke(
+        input=[{"role": "system", "content": "You are a helpful assistant"}, *st.session_state.chat_history]
+    )
+    assistant_response = response.content
+    st.session_state.chat_history.append({"role": "assistant", "content": assistant_response})
+    with st.chat_message("assistant"):
+        st.markdown(assistant_response)
